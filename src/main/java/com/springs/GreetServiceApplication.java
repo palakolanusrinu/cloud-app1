8d0778c59498c05e4c6b90fdb4fd89d1a9c9b8bb
Srinivasa Reddy <reddy.grails@gmail.com>
2025-01-23 12:30:14 +0530

    commit

--- src/main/java/com/springs/GreetServiceApplication.java
+++ src/main/java/com/springs/GreetServiceApplication.java
@@ -2,12 +2,20 @@ package com.springs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController("/api")
 public class GreetServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GreetServiceApplication.class, args);
+	}
+	
+	@RequestMapping("/greet")
+	public String greet() {
+		return "good to see!!!";
 	}
 
 }
